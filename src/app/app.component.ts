634ffe9bd79b08e45775d93841345c40ef9bdbbc
bartlomiejszwarc <szwarc.it@gmail.com>
2023-04-21 12:35:30 +0200

    Added sidenav for mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from './services/auth.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +10,8 @@ import {AuthService} from './services/auth.service';
 export class AppComponent implements OnInit {
   title = 'imitter';
   constructor(private authService: AuthService, public router: Router) {}
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  opened!: boolean;
 
   ngOnInit(): void {
     this.authService.getUserById();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,40 @@
-import {environment} from './../environments/environment';
-import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {LoginPageComponent} from './components/login-page/login-page.component';
-import {LoginWindowComponent} from './components/login-window/login-window.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {SignupPageComponent} from './components/signup-page/signup-page.component';
-import {ListComponent} from './components/list/list.component';
-import {DashboardPageComponent} from './components/dashboard-page/dashboard-page.component';
-import {DashboardSidebarComponent} from './components/dashboard-sidebar/dashboard-sidebar.component';
-import {DashboardCreateComponent} from './components/dashboard-create/dashboard-create.component';
-import {PostComponent} from './components/post/post.component';
-import {DashboardSearchComponent} from './components/dashboard-search/dashboard-search.component';
-import {DashboardTrendsComponent} from './components/dashboard-trends/dashboard-trends.component';
-import {MatIconModule} from '@angular/material/icon';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {AuthInterceptor} from './services/auth-interceptor';
-import {ProfilePageComponent} from './components/profile-page/profile-page.component';
-import {EditProfileComponent} from './components/edit-profile/edit-profile.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {ProfileUserDetailsComponent} from './components/profile-user-details/profile-user-details.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {PostLikesListComponent} from './components/post-likes-list/post-likes-list.component';
-import {UserLikesComponent} from './components/user-likes/user-likes.component';
-import {UserNotFoundComponent} from './components/user-not-found/user-not-found.component';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DashboardRightPanelComponent} from './components/dashboard-right-panel/dashboard-right-panel.component';
+import { environment } from './../environments/environment';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LoginWindowComponent } from './components/login-window/login-window.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { ListComponent } from './components/list/list.component';
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
+import { DashboardSidebarComponent } from './components/dashboard-sidebar/dashboard-sidebar.component';
+import { DashboardCreateComponent } from './components/dashboard-create/dashboard-create.component';
+import { PostComponent } from './components/post/post.component';
+import { DashboardSearchComponent } from './components/dashboard-search/dashboard-search.component';
+import { DashboardTrendsComponent } from './components/dashboard-trends/dashboard-trends.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth-interceptor';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ProfileUserDetailsComponent } from './components/profile-user-details/profile-user-details.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { PostLikesListComponent } from './components/post-likes-list/post-likes-list.component';
+import { UserLikesComponent } from './components/user-likes/user-likes.component';
+import { UserNotFoundComponent } from './components/user-not-found/user-not-found.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardRightPanelComponent } from './components/dashboard-right-panel/dashboard-right-panel.component';
 import { FollowListComponent } from './components/follow-list/follow-list.component';
 import { ProfileDetailsCardComponent } from './components/profile-details-card/profile-details-card.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -68,8 +71,11 @@ import { ProfileDetailsCardComponent } from './components/profile-details-card/p
     MatMenuModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
